3d25dcb43245b9c920154b24451cf36bf2316dac
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2017-11-01 14:16:21 +0530

    Turns DEBUG true, corrects urlpatterns

--- Farmers_Portal/urls.py
+++ Farmers_Portal/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
